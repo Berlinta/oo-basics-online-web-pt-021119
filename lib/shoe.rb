e48b286e38d0b6f622c5e0033194038d8dfc9178
Nash Brancic <nashfft@gmail.com>
2019-03-01 20:56:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,51 +1,13 @@
 class Shoe
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+  
   def initialize(brand)
     @brand = brand
-  end
-  
-  def brand=(make)
-    @brand = make
-  end
-  
-  def brand
-    @brand
-  end
-  
-  def color=(pigment)
-    @color = pigment
-  end
-
-  def color
-    @color
-  end
-  
-  def size=(dimension)
-    @size = dimension
-  end
-  
-  def size
-    @size
-  end
-  
-  def material=(matter)
-    @material = matter
-  end
-  
-  def material
-    @material
-  end
-  
-  def condition=(state)
-    @condition = state
-  end
-  
-  def condition
-    @condition
   end
   
   def cobble
     puts "Your shoe is as good as new!"
     @condition = "new"
   end
-
 end
