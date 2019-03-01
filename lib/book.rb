0dbd42e7c9d8f407d7b96d867dfeea1e5a00347c
Nash Brancic <nashfft@gmail.com>
2019-03-01 20:39:42 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -15,7 +15,20 @@ class Book
     @author
   end
   
-  def page_count
-          
+  def page_count=(page)
+    @page_count = page
+  end
   
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
+          
 end
